c20d955718a22aee0815da5b2bdc597ee0df63a0
Michelle Steigerwalt <msteigerwalt@gmail.com>
2019-12-12 11:40:49 -0500

    Styling tweaks and whitespace changes.
    Addresses Adobe review concerns.

--- src/panel.js
+++ src/panel.js
@@ -1,26 +1,38 @@
 /* Mixing HTML and JavaScript is gross. */
 module.exports =`
 <style type="text/css">
+	#modal {
+		width: 100%;
+		max-width: 250px;
+	}
 	#modal #buttons { display:none; }
 	#modal.selection #instructions { display: none; }
 	#modal.selection #buttons { display: block; }
 	#instructions {
-		height: 250px;
-		width: 250px;
-		padding-top: 274px;
 		justify-content: center;
 		align-items: center;
-		background: url('./i/confused_guy.png') no-repeat;
+		width: 100%;
+	}
+	img {
+		width: 100%;
+		height: auto;
+		max-width: 250px;
+		padding-bottom: 24px;
+	}
+	label {
+		display: flex;
+		justify-content: space-between;
 	}
 </style>
 <div id="modal">
 	<div id="instructions">
+		<img src="./i/confused_guy.png" />
 		<p>Select a text node to randomize social placeholders.</p>
 	</div>
 	<div id="buttons">
-		<button data-generator="username">@username</button><br/>
-		<button data-generator="fullname">Full Name</button><br/>
-		<button data-generator="moji">😃 Emoji Text</button><br/>
-	</div>
+			<button data-generator="username">@username</button><br/>
+			<button data-generator="fullname">Full Name</button><br/>
+			<button data-generator="moji">😃 Emoji Text</button><br/>
+		</div>
 </div>
 `;
